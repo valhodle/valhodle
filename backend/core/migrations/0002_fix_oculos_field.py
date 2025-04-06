ea23412eddc2a4921097540c3ddd2a9d7f5017d6
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2025-04-06 15:29:51 -0300

    Update 0002_fix_oculos_field.py

--- backend/core/migrations/0002_fix_oculos_field.py
+++ backend/core/migrations/0002_fix_oculos_field.py
@@ -1,28 +1,24 @@
 from django.db import migrations, connection
 
-
 def alter_oculos_to_jsonb(apps, schema_editor):
     if connection.vendor == 'postgresql':
+        schema_editor.execute("""
+            ALTER TABLE core_pessoa
+            ALTER COLUMN oculos DROP DEFAULT,
+            ALTER COLUMN oculos DROP NOT NULL,
+            ALTER COLUMN oculos TYPE text USING oculos::text;
+        """)
         schema_editor.execute("""
             ALTER TABLE core_pessoa
             ALTER COLUMN oculos TYPE jsonb USING oculos::jsonb;
         """)
 
-
-def reverse_alter_oculos_to_boolean(apps, schema_editor):
-    if connection.vendor == 'postgresql':
-        schema_editor.execute("""
-            ALTER TABLE core_pessoa
-            ALTER COLUMN oculos TYPE boolean USING oculos::boolean;
-        """)
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
+        ("core", "0001_initial"),
     ]
 
     operations = [
-        migrations.RunPython(alter_oculos_to_jsonb, reverse_alter_oculos_to_boolean),
+        migrations.RunPython(alter_oculos_to_jsonb),
     ]
